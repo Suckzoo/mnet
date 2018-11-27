017c7d0d3af7bf3bc482a6bd2daefcc6f9b9be0e
kevins3204 <kevins3204@gmail.com>
2018-11-27 09:26:13 +0900

    nested java codes

---
+++ testcodes/manual.java
@@ -0,0 +1,56 @@
+public class Test {
+  public static void main(String args[]) {
+    int x = 30;
+    int y = 10;
+    int z = 15;
+
+    if(x == 30) {
+      if(y == 10) {
+        if(z == 15) {
+          System.out.print("X = 30 and Y = 10 and Z == 15");
+        }
+      }
+    }
+    if(x > 0){
+      if(x < 35){
+        if(y < 0){
+          if(z > 30){
+            System.out.print("1");;
+          }
+          else if(z < 30){
+            System.out.print("2");;
+          }
+        }
+        else if(y > 0){
+          if(z > 30){
+            System.out.print("3");;
+          }
+          else if(z < 30){
+            System.out.print("4");;
+          }
+        }
+      }
+      else if(x > 35){
+        if(y < 0){
+          if(z > 30){
+            System.out.print("5");;
+          }
+          else if(z < 30){
+            System.out.print("6");;
+          }
+        }
+        else if(y > 0){
+          if(z > 30){
+            System.out.print("7");;
+          }
+          else if(z < 30){
+            System.out.print("8");;
+          }
+        }
+      }
+      else{
+        System.out.print("NA");
+      }
+    }
+  }
+}
