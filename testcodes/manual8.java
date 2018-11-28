8e6c4d92c72432bd94e327f36612033e8549f1c8
kevins3204 <kevins3204@gmail.com>
2018-11-29 02:52:20 +0900

    manual8

---
+++ testcodes/manual8.java
@@ -0,0 +1,58 @@
+public class manual8 {
+
+  public int testMe(int x, int y, int z, int a, int b, int c, int p, int q, int r, int k) {
+    if(x + 2 * y > 0) {
+      if(2 * x + 3 * y - z > 0){
+        if(x + y - z > 0){
+          if(x > 0){
+            if(a * b - c > 0){
+              if(2 * b + 3 * c > a){
+                if(a + b > c){
+                  if(b > c){
+                    if(p - q + r > 0){
+                      if(p > q){
+                        if(3 * q + 2 * r - p > 0){
+                          System.out.prinln("a");
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    if(x + 2 * y > 0) {
+      if(2 * x + 3 * y - z > 0){
+        if(x + y - z > 0){
+          if(x > 0){
+            if(a * b - c > 0){
+              if(2 * b + 3 * c > a){
+                if(a + b > c){
+                  if(b + k > c){
+                    if(p - q + r + k > 0){
+                      if(p > q - k){
+                        if(3 * q + 2 * r - p > 0){
+                          if(k < 0){
+                            if(2 * x - k > 0){
+                              if(33 * y + k > 0){
+                                System.out.prinln("b");
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return 0;
+  }
+}
