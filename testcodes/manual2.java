c12168015e37495719bfb86459630651f3c80d67
kevins3204 <kevins3204@gmail.com>
2018-11-29 01:53:22 +0900

    Manual test cases modified/added

--- testcodes/manual2.java
+++ testcodes/manual2.java
@@ -1,31 +1,5 @@
 public class manual2 {
-  public void TestMe() {
-    int a = 0;
-    int b = 1;
-    int c = 2;
-    int d = 3;
-    int e = 4;
-    int f = 5;
-    int g = 6;
-    int h = 7;
-    int i = 8;
-    int j = 9;
-    int k = 10;
-    int l = 11;
-    int m = 12;
-    int n = 13;
-    int o = 14;
-    int p = 15;
-    int q = 16;
-    int r = 17;
-    int s = 18;
-    int t = 19;
-    int u = 20;
-    int v = 21;
-    int w = 22;
-    int x = 23;
-    int y = 24;
-    int z = 25;
+  public int testMe(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y, int z) {
     if(a == b){
       if(c == d){
         if(e == f){
@@ -51,23 +25,10 @@ public class manual2 {
                                                 if(s == u){
                                                   if(u == w){
                                                     if(w == y){
-                                                      System.out.println("All alphabets are same");
-                                                    }
-                                                    else{
-                                                      System.out.println("Some are different");
+                                                      System.out.println("All alphabets have same values!");
                                                     }
                                                   }
                                                 }
-                                              }
-                                            }
-                                          }
-                                          else{
-                                            if(a + b + c == 0){
-                                              if(a - b - c == 0){
-                                                System.out.prinln("Ho");
-                                              }
-                                              else{
-                                                System.out.prinln("What");
                                               }
                                             }
                                           }
@@ -90,5 +51,6 @@ public class manual2 {
         }
       }
     }
+    return 0;
   }
 }
--- testcodes/manual4.java
+++ testcodes/manual4.java
@@ -1,28 +1,17 @@
 public class manual4 {
-  public void TestMe() {
+  public void testMe(int k, int g) {
     String a = "Babari";
     String b = "Chiwawa";
     Stirng c = "Hammaya";
     String d = "Io";
     String e = "Sono";
     String f = "Studento";
-    if(a.length() > 0){
-      if(b.length() < 0){
-        System.out.prinln("This is not good");
-      }
-      else{
-        if(c == "Hammaya"){
-          if(d.length() > 0 && e.length() > 0 || f.length() > 0){
-            System.out.prinln(d + e + f);
-          }
-          if(a + b == "aaa"){
-            System.out.prinln("good");
-          }
-          else{
-            if(a.length() > 0){
-              if(d.length() > 0){
-                System.out.prinln("Give us A+");
-              }
+    if(k == 1){
+      if(g == 5){
+        if(a.length() > 0){
+          if(c == "Hammaya"){
+            if(d.length() > 0 && e.length() > 0 || f.length() > 0){
+              System.out.prinln(d + e + f);
             }
           }
         }
