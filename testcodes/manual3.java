bb94237f8d11ae6d57e9d8c8cae6eeb5f2c3bbc8
kevins3204 <kevins3204@gmail.com>
2018-11-29 02:33:34 +0900

    manual3 nested

--- testcodes/manual3.java
+++ testcodes/manual3.java
@@ -1,13 +1,21 @@
 public class manual3 {
   public void testMe(int a, int b, int c, int d, int x, int y, int z) {
-    if(a == b && c == d){
-      if(a > c && b > d && x == y || z > a){
-        if(a == 0){
-          if(a + b - c < 0){
-            if(((b + d) - c) * a == 0){
-              if(a >= 0){
-                if(x == 23){
-                  System.out.prinln("Buona Sera");
+    if(a == b){
+      if(c == d){
+        if(a > c){
+          if(b > d){
+            if(x == y){
+              if(z > a){
+                if(a == 0){
+                  if(a + b - c < 0){
+                    if(((b + d) - c) * a == 0){
+                      if(a >= 0){
+                        if(x == 23){
+                          System.out.prinln("Buona Sera");
+                        }
+                      }
+                    }
+                  }
                 }
               }
             }
