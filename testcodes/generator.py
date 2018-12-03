6413a4f7f0f88878bcac70ea50af9f8749f03c7b
kevins3204 <kevins3204@gmail.com>
2018-12-03 17:58:30 +0900

    test generator/manual_12_5

---
+++ testcodes/generator.py
@@ -0,0 +1,43 @@
+from random import *
+import random
+x1 = random.random()*100
+x2 = random.random()*100
+x3 = random.random()*100
+x4 = random.random()*100
+x5 = random.random()*100
+x6 = random.random()*100
+x7 = random.random()*100
+x8 = random.random()*100
+x9 = random.random()*100
+x10 = random.random()*100
+x11 = random.random()*100
+x12 = random.random()*100
+for i in range(5):
+  a1 = randint(1, 100)
+  a2 = randint(1, 100)
+  a3 = randint(1, 100)
+  a4 = randint(1, 100)
+  a5 = randint(1, 100)
+  a6 = randint(1, 100)
+  a7 = randint(1, 100)
+  a8 = randint(1, 100)
+  a9 = randint(1, 100)
+  a10 = randint(1, 100)
+  a11 = randint(1, 100)
+  a12 = randint(1, 100)
+  print('x1 * '+str(a1) + ' + x2 * '+str(a2) + ' + x3 * '+str(a3) + ' + x4 * '+str(a4) + ' + x5 * '+str(a5) + ' + x6 * '+str(a6) + ' + x7 * '+str(a7) + ' + x8 * '+str(a8) + ' + x9 * '+str(a9) + ' + x10 * '+str(a10) + ' + x11 * '+str(a11) + ' + x12 * '+str(a12), end=" ")
+  #print(str(x1)+' * '+str(a1)+' + '+str(x2)+' * '+str(a2)+' + '+str(x3)+' * '+str(a3)+' + '+str(x4)+' * '+str(a4)+' + '+str(x5)+' * '+str(a5)+' + '+str(x6)+' * '+str(a6)+' + '+str(x7)+' * '+str(a7)+' + '+str(x8)+' * '+str(a8)+' + '+str(x9)+' * '+str(a9)+' + '+str(x10)+' * '+str(a10)+' + '+str(x11)+' * '+str(a11)+' + '+str(x12)+' * '+str(a12), end=" ")
+  print('>', end=" ")
+  print(x1*a1+x2*a2+x3*a3+x4*a4+x5*a5+x6*a6+x7*a7+x8*a8+x9*a9+x10*a10+x11*a11+x12*a12)
+print('x1 = '+str(x1))
+print('x2 = '+str(x2))
+print('x3 = '+str(x3))
+print('x4 = '+str(x4))
+print('x5 = '+str(x5))
+print('x6 = '+str(x6))
+print('x7 = '+str(x7))
+print('x8 = '+str(x8))
+print('x9 = '+str(x9))
+print('x10 = '+str(x10))
+print('x11 = '+str(x11))
+print('x12 = '+str(x12))
