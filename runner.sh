61c59071f24201c8898e24e0bd86727bff2cd02f
Seokju Hong <tjkj555@gmail.com>
2018-12-05 21:26:43 +0900

    modify runner script to repeat 20 times

--- runner.sh
+++ runner.sh
@@ -19,21 +19,25 @@ get_testfile()
   testfile="manual_$1_$2.txt"
 }
 
-for runner in traditional.py $(ls mnet/)
+for trial in 1 .. 20
 do
-  get_runner $runner
-  echo "\n\nRunning $actual_runner..."
-  for numvar in 9 12
+  echo "\n\nTrial $trial..."
+  for runner in traditional.py $(ls mnet/)
   do
-    for deepness in 5 10 15 20
+    get_runner $runner
+    echo "\n\nRunning $actual_runner..."
+    for numvar in 3
     do
-      get_testfile $numvar $deepness
-      runner_directory=$(echo $runner | cut -d'.' -f 1)
-      log_directory="log/$runner_directory/$testfile"
-      mkdir -p $log_directory
-      echo "[$actual_runner] Running on $testfile..."
-      echo "./run.sh $actual_runner $testfile"
-      ./run.sh $actual_runner "testcodes/results/$testfile" > $log_directory/stdout 2> $log_directory/stderr
+      for deepness in 5 10 15 20
+      do
+        get_testfile $numvar $deepness
+        runner_directory=$(echo $runner | cut -d'.' -f 1)
+        log_directory="trial_$trial/$runner_directory/$testfile"
+        mkdir -p $log_directory
+        echo "[$actual_runner] Running on $testfile..."
+        echo "./run.sh $actual_runner $testfile"
+        ./run.sh $actual_runner "testcodes/results/$testfile" > $log_directory/stdout 2> $log_directory/stderr
+      done
     done
   done
 done
